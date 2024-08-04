6cd4c5ff0b6f659fdebf735882d5495ca6825136
alwaysArg <coutodiego.f@outlook.com>
2024-08-04 19:58:16 +0200

    added table, started ot do ply manager.

--- src/app/pages/ousia-page/ousia-page.component.ts
+++ src/app/pages/ousia-page/ousia-page.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+
+
+interface PlyDatabase {
+  value: string;
+  viewValue: string;
+}
+
+
+
 @Component({
   selector: 'app-ousia-page',
   templateUrl: './ousia-page.component.html',
   styleUrl: './ousia-page.component.scss'
 })
 export class OusiaPageComponent {
-
+  plyDatabasesAll: PlyDatabase[] = [
+    { value: 'database-0', viewValue: 'Boat' },
+    { value: 'database-1', viewValue: 'Silo' },
+    { value: 'database-2', viewValue: 'Bridges' },
+  ];
 }
